c70cb71d2bb352d5adb2a95dc5d5cafad158af6a
Alexandra Pshebitkovskaya <alexandrapshebitkovskaya@gmail.com>
2021-01-17 22:42:27 +0200

    add js file

---
+++ source/js/nav.js
@@ -0,0 +1,17 @@
+const navMain = document.querySelector('.nav');
+const navToggle = document.querySelector('.nav__button');
+const pageHeader = document.querySelector('.page-header')
+
+navMain.classList.remove('nav--nojs');
+
+navToggle.addEventListener('click', function() {
+  if (navMain.classList.contains('nav--closed')) {
+    pageHeader.classList.remove('page-header--closed');
+    navMain.classList.remove('nav--closed');
+    navMain.classList.add('nav--opened');
+  } else {
+    pageHeader.classList.add('page-header--closed');
+    navMain.classList.add('nav--closed');
+    navMain.classList.remove('nav--opened');
+  }
+});
